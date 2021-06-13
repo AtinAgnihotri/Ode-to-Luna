934b31d28f13dcf5b5a4f503a5374d835b68a5d7
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-13 17:49:33 +0530

    Initial Commit

---
+++ Ode to Luna/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ode to Luna
+//
+//  Created by Atin Agnihotri on 13/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ode to Luna/Ode_to_LunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ode_to_LunaApp.swift
+//  Ode to Luna
+//
+//  Created by Atin Agnihotri on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Ode_to_LunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
