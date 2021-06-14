f433e90003a1fec4a5a7c89f2d5b13b8f08d33cc
AtinAgnihotri <atinagnihotri@gmail.com>
2021-06-14 22:35:27 +0530

    Add CrewMemberView

--- Ode to Luna/Views/CrewMemberView.swift
+++ Ode to Luna/Views/CrewMemberView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct CrewMemberView: View {
+    var crewMember: CrewMemberModel
+    
+    init(_ crewMember: CrewMemberModel) {
+        self.crewMember = crewMember
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(crewMember.astronaut.id)
+                .resizable()
+                .frame(width: 80, height: 50)
+                .clipShape(Capsule())
+                .overlay(Capsule()
+                            .stroke(Color.primary, lineWidth: 1)
+                )
+            
+            VStack (alignment: .leading) {
+                Text(crewMember.astronaut.name)
+                    .font(.headline)
+                    .foregroundColor(.primary)
+                Text(crewMember.role)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+        }
+        .padding(.horizontal)
     }
 }
 
 struct CrewMemberView_Previews: PreviewProvider {
+    static let astronauts: [AstronautModel] = Bundle.main.decode("astronauts.json")
+    static let crewMember = CrewMemberModel(role: "pilot", astronaut: astronauts[0])
+    
     static var previews: some View {
-        CrewMemberView()
+        CrewMemberView(crewMember)
     }
 }
