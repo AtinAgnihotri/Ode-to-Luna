ef67f2f6097c40475d304d988d3462b024eb3adb
AtinAgnihotri <atinagnihotri@gmail.com>
2021-06-14 22:34:35 +0530

    Add CreditsView

--- Ode to Luna/Views/CreditsView.swift
+++ Ode to Luna/Views/CreditsView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct CreditsView: View {
+    let nasaCredit = "The images of astronauts and mission badges by NASA, used under public domain license as per Title 17, Chapter 1, Section 105 of the US Code."
+    let wikipediaCredit = "Credit for info on Astronauts and Apollo Missions : Wikipedia and its authors, licensed under CC-BY-SA available from here:\nhttps://creativecommons.org/licenses/by-sa/3.0"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("NASA").font(.headline)) {
+                Text(self.nasaCredit)
+                    .padding()
+            }
+            Section(header: Text("WIKIPEDIA").font(.headline)) {
+                Text(self.wikipediaCredit)
+                    .padding()
+            }
+        }.navigationBarTitle("Credits", displayMode: .inline)
     }
 }
 
